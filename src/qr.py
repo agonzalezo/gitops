3126e31c85d18a6e51f1dda9d7b40ee4a73c7fdd
Alex <mexi1415@gmail.com>
2024-08-17 18:30:43 -0500

    add qr functions

--- src/qr.py
+++ src/qr.py
@@ -1,3 +1,11 @@
 import io
 import qrcode
 import os
+
+if __name__ == '__main__':
+    qr = qrcode.QRCode()
+    qr.add_data("hola mundo desde github")
+    file = io.StringIO()
+    qr.print_ascii(out=file)
+    file.seek(0)
+    print(file.read())
